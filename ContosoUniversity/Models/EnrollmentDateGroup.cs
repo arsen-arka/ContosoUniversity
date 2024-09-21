a97c8f800067700f0887129602f822563866f698
Arsen Karapetyan <arsen.karapetyan@gmail.com>
2024-09-21 13:17:23 +0200

    Created complex  data model

---
+++ ContosoUniversity/Models/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class EnrollmentDateGroup
+    {
+        public int EnrollmentDateGroupID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System; 
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string? FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
